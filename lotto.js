9cff7fa29e7524459ebd47d482b33e7cf9f7ad5d
Moon-Sangho <skym887@gmail.com>
2020-07-21 22:50:54 +0900

    3rd commit

--- lotto.js
+++ lotto.js
@@ -19,3 +19,20 @@ const win = shuffle.slice(0, 6);
 const bonus = shuffle[shuffle.length - 1];
 
 console.log('Win number :', win.sort(function (p, c) {return p - c}), 'bonus :', bonus);
+
+// 화면에 당첨 번호 출력
+const winBall = document.getElementById('winBall');
+
+for (let i = 0; i < win.length; i++) {
+    const winNumber = document.createElement('div');
+    winNumber.textContent = win[i];
+    winBall.appendChild(winNumber);
+}
+
+// 화면에 보너스 번호 출력
+const bonusBall = document.getElementById('bonusBall');
+const bonusNumber = document.createElement('div');
+
+bonusNumber.textContent = bonus;
+bonusBall.appendChild(bonusNumber);
+
